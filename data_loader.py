9af213e4d439493a7a3024f207c574cbcf474f0c
Valeria Vibe <nverlivs@mail.ru>
2025-10-01 13:12:14 +0300

    made 3d task

--- data_loader.py
+++ data_loader.py
@@ -1,8 +1,24 @@
 import pandas as pd
 
-file_id = "1i_H46ci0vi-B17v8UKIGfpQeGRpaa2uI"  # ID файла на Google Drive
+file_id = "1i_H46ci0vi-B17v8UKIGfpQeGRpaa2uI"
 file_url = f"https://drive.google.com/uc?id={file_id}"
 
-raw_data = pd.read_csv(file_url)     # читаем файл
+df = pd.read_csv(file_url)
 
-print(raw_data.head(10))      # выводим на экран первые 10 строк для проверки
+print('Типы данных до изменения:', df.dtypes)
+
+df[['created_at', 'updated_at']] = df[['created_at', 'updated_at']].apply(
+    pd.to_datetime, errors='coerce', utc=True)
+df[['state', 'origin', 'carcinogenicity', 'export']] = df[['state', 'origin', 'carcinogenicity', 'export']].astype('category')
+df = df.drop(columns=['Unnamed: 0'])
+
+num_cols = ['weight', 'moldb_average_mass', 'moldb_mono_mass', 'logp']
+df[num_cols] = df[num_cols].apply(pd.to_numeric, errors='coerce')
+
+text = ['title', 'common_name', 'description', 'appearance',
+'solubility', 'route_of_exposure', 'mechanism_of_toxicity', 'metabolism',
+'toxicity', 'lethaldose', 'use_source', 'health_effects', 'symptoms', 'treatment']
+df[text] = df[text].astype(str)
+
+print('Типы данных после изменения:', df.dtypes)
+
